6e04e6c62325ddea62663ce82fdb8c71916e0b72
qYogi <bogdanviziru18@gmail.com>
2025-03-13 11:58:54 +0200

    minor styling adjustments

--- app/_components/whatsapp/PhoneIcon.tsx
+++ app/_components/whatsapp/PhoneIcon.tsx
@@ -6,12 +6,12 @@ interface PhoneIconProps {
 }
 
 export const PhoneIcon: React.FC<PhoneIconProps> = ({
-  color = "#1C274C",
+  color = "#31427c",
   className = "",
 }) => {
   return (
     <div
-      className={`fixed bottom-4 left-4 z-50 inline-flex items-center justify-center w-14 h-14 rounded-full md:hidden bg-primary ${className}`}
+      className={`fixed bottom-4 right-4 z-50 inline-flex items-center justify-center w-14 h-14 rounded-full md:hidden bg-primary ${className}`}
     >
       <svg
         width="36px"
